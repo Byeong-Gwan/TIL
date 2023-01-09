de299fc86bcfb023a4cded466d70c3c0220823e2
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-01-09 14:54:24 +0900

    스코프, 호이스팅 // 데이터 통신 // 구조분해할당 API활용

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
 const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
 const saveItemsFn = function () {
@@ -63,7 +64,7 @@ if (savedTodoList) {
 }
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     // keyCode Enter 키 값이 13 Enter 선택 시 이벤트 발생
     createTodo();
   }
@@ -77,3 +78,76 @@ const deleteAll = function () {
   }
   saveItemsFn();
 };
+
+const weatherDataActive = function ({ location, weather }) {
+  const weatherMainList = [
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "ThunderStorm",
+  ];
+  weather = weatherMainList.includes(weather) ? weather : "Fog";
+  const locationNameTag = document.querySelector("#location-name-tag");
+
+  locationNameTag.textContent = location;
+  console.log(weather);
+  document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+
+  if (
+    !savedWeatherData ||
+    savedWeatherData.location !== location ||
+    savedWeatherData.weather !== weather
+  ) {
+    console.log("조건식 성립");
+    localStorage.setItem(
+      "saved-weather",
+      JSON.stringify({ location, weather })
+    );
+  }
+};
+
+//  Geolocation API로 현재 위치 가져오기
+// latitude : 위도 // longitude : 경도
+const weatherSearch = function ({ latitude, longitude }) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4d0a0d3ca4914e6c0abcd6bce36848d5`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].main);
+      const weatherData = {
+        location: json.name,
+        weather: json.weather[0].main, // 현재 날씨 마춰서 변경
+      };
+      weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const accessToGeo = function ({ coords }) {
+  // console.log(typeof position); // position의 type : Object
+  // console.log(position.coords.longitude); // 경도
+  // console.log(position.coords.latitude); // 위도
+  const { latitude, longitude } = coords;
+  // shorthand property keys name : values 값의 이름이 같으면 keys name 생략 가능
+  const positionObj = {
+    // Object : 위도(latitude) && 경도(longitude)
+    latitude,
+    longitude,
+  };
+
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
+};
+askForLocation();
