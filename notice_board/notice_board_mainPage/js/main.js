a00a3a91249ea37c61890061c4a365f0eecdb7e9
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-03-22 18:15:27 +0900

    게시판 메인 페이지 만들고, 작성하기 페이지 연결
    아직 작성된 데이터 값을 게시판 메인(목록)창에 가져오는 기능은 미완성

---
+++ notice_board/notice_board_mainPage/js/main.js
@@ -0,0 +1,5 @@
+// index.js 파일에서 실행되는 코드
+const pageGoBtn = document.querySelector(".page-go");
+pageGoBtn.addEventListener("click", () => {
+  window.location.href = "../../notice_board_writePage/writePage.html";
+});
