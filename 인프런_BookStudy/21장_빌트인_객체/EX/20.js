513bae5958c5f374134660df03a4dcaf7643d477
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-04-03 13:41:02 +0900

    22장 this 정리

---
+++ 인프런_BookStudy/21장_빌트인_객체/EX/20.js
@@ -0,0 +1,21 @@
+/**
+ * 전달받은 인수가 유한수인지 확인하고 그 결과를 반환한다.
+ * @param {number} testValue - 검사 대상 값
+ * @returns {boolean} 유한수 여부 확인 결과
+ */
+isFinite(testValue)
+
+// 인수가 유한수이면 true를 반환한다.
+isFinite(0);    // true
+isFinite(2e64); // true
+isFinite('10'); // true: '10' -> 10
+isFinite(null); // true: null -> 0
+
+// 인수가 무한수 또는 NaN으로 평가되는 값이라면 false를 반환한다.
+isFinite(Infinity); // false
+isFinite(-Infinity); // false
+
+// 인수가 NaN으로 평가되는 값이라면 false를 반환한다.
+isFinite(NaN);      // false
+isFinite('Hello')   // false
+isFinite('2005/12/12'); // false
