d0a3ae4ee35326496a801a97dc621db5fa187c6d
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-05-15 18:19:52 +0900

    svelte 프레임워크 공부

---
+++ svelte-test/src/First.svelte
@@ -0,0 +1,33 @@
+<script>
+	let name = 'world';
+	let age = 85;
+	setTimeout(() => {
+		name = 'Heropy',
+		age = 36
+	}, 3000);
+	
+	function assign() {
+		name = 'Beyong-gwan',
+		age = 33
+	};
+</script>
+
+<h1>Hello {name}!</h1>
+<h2 class={age < 85 ? 'active' : ''}>
+	{age}
+</h2>
+<img src="" alt = {name} />
+<input type="text" bind:value={name} />
+<button on:click = {assign}>
+	Assion
+</button>
+
+<style>
+	h1 {
+		color: red;
+	}
+	
+	.active {
+		color: blue;
+	}
+</style>
