c12acca3a53d5d981e7947f562a39e8f7cc0a4c1
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-05-16 13:36:22 +0900

    svelte 기초 공부

---
+++ svelte-test/src/Store/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import {storeName} from './store.js';
+	import Parent from './Parent.svelte';
+	
+	let name = 'World';	
+// 	console.log(storeName);
+// 	console.log($storeName);
+	$storeName = name; // store 객체의 데이터로 사용
+</script>
+
+<h1>
+	Hello {name}!
+</h1>
+<Parent />
