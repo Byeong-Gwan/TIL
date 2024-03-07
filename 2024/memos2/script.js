553361ed04a0bfdc53a15f957cc25c3c63a59477
Byeong-Gwan <ansd_my3073@naver.com>
2024-03-08 00:37:49 +0900

    memo index, text, date 추가 수정 (미완성)

--- 2024/memos2/script.js
+++ 2024/memos2/script.js
@@ -9,6 +9,7 @@ let memos = [];
 // event
 function bindEvent() {
     document.getElementById('save-btn').addEventListener('click', saveMemo);
+    // document.getElementById('
 }
 
 // 저장 버튼 클릭시 실행 함수
@@ -17,31 +18,52 @@ function saveMemo() {
     const memoText = memoInput.value;
 
     if (memoText.trim() !== '') {
-        // 신규 메모 배열에 추가 
-        memos.push(memoText);
+
+        // 객체 생성 index, text, Data 추가
+        const newMemo = {
+            index: memos.length,
+            text: memoText,
+            date: new Date().toLocaleString()
+        };
+
+        // 신�� 메모 배열에 추가 
+        memos.push(newMemo);
+
+        memos.sort((a, b) => a.date - b.date);
 
         renderMemoList();
 
-        // 입력창 초기화
         memoInput.value = '';
+
     }
 }
 
 // 메모 삭제 함수
 function deleteMemo(index) {
     memos.splice(index, 1);
+    addIndex();
     renderMemoList();
 }
 
 // 메모 수정 함수
 function editMemo(index) {
-    const memoText = prompt('메모를 수정하세요: ', memos[index]);
+    const memoIndex = memos[index]
+    const memoText = prompt('메모를 수정하세요: ', memoIndex.text);
     if (memoText !== null && memoText.trim() !== '') {
-        memos[index] = memoText;
+        // addIndex(index, memoText)
         renderMemoList();
     }
 
 }
+
+// index 값 추가 함수
+function addIndex() {
+    memos.forEach((memo, index) => {
+        memo.index = index;
+    })
+}
+
+
 
 
 // 메모 목록을 표시하는 함수
@@ -53,9 +75,24 @@ function renderMemoList() {
     memos.forEach((memo, index) => {
         // 저장 클릭 시 div 태그 추가 작성된 내용 저장
         const memoItem = document.createElement('div');
-        memoItem.textContent = memo;
         memoItem.classList.add('memo');
         memoList.appendChild(memoItem); // 'memo-list' 자식 으로 div 생성
+
+        // span 태그로 index, text, date 로 생성해서 추가
+        const memoIndex = document.createElement('span');
+        memoIndex.textContent = `${memo.index + 1}`;
+        memoIndex.classList.add('index');
+        memoItem.appendChild(memoIndex);
+
+        const memoText = document.createElement('span');
+        memoText.textContent = `${memo.text}`;
+        memoText.classList.add('text');
+        memoItem.appendChild(memoText);
+
+        const memoDate = document.createElement('span');
+        memoDate.textContent = `${memo.date}`;
+        memoDate.classList.add('data');
+        memoItem.appendChild(memoDate);
 
         // 삭제 버튼 추가
         const deleteButton = document.createElement('button');
